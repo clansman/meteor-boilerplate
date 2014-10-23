4c0b6f1c6f48e26e8eb7091c77a51a959dec8da3
Zulfat Ilyasov <mail@zulfat.net>
2014-10-24 01:07:02 +0400

    initial

---
+++ routes.js
@@ -0,0 +1,54 @@
+Router.map(function() {
+
+    this.route('home', {
+        path: '/'
+    });
+
+    this.route('dashboard', {
+        path: '/dashboard',
+        loginRequired: 'entrySignIn',
+        waitOn: function() {
+            return this.subscribe("items");
+        },
+        data: {
+            items: Items.find({})
+        },
+        onAfterAction: function() {
+            SEO.set({
+                title: 'Dashboard | ' + SEO.settings.title
+            });
+        }
+    });
+
+    this.route('profile', {
+        path: '/profile',
+        data: function() {
+            return Meteor.user();
+        }
+    });
+
+    this.route('articles', {
+        path: '/articles',
+        data: function() {
+            return {
+                articles: [{
+                    Title: 'Article title',
+                    Content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, rem voluptatum explicabo sint adipisci, aliquam suscipit sed culpa reiciendis iure itaque incidunt excepturi ad pariatur voluptates! Quod culpa ea excepturi.'
+                }, {
+                    Title: 'Another Cool Story',
+                    Content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint nam iste doloribus itaque cupiditate, voluptates error reprehenderit, consectetur ratione unde iure. Possimus quam voluptatem dicta est doloremque cumque eos voluptatum!'
+                }]
+            };
+        }
+    });
+
+    this.route('notFound', {
+        path: '*',
+        where: 'server',
+        action: function() {
+            this.response.statusCode = 404;
+            this.response.end(Handlebars.templates['404']());
+        }
+    });
+
+});
